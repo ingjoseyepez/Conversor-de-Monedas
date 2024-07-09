6a8be3d4ba8e76b10b6a7b52d74b0cc58ccdcd06
jose Yepez <ing.joseyepez@gmail.com>
2024-07-09 15:54:23 -0500

    [create] Interacion con el usuario

--- src/com/challegen/converso/modelos/Conversion.java
+++ src/com/challegen/converso/modelos/Conversion.java
@@ -1,9 +1,18 @@
 package com.challegen.converso.modelos;
 
+import com.challegen.converso.service.ConexionApis;
+
+import java.util.Scanner;
+
 public class Conversion {
     private String monedaOrigen;
     private String monedaDestino;
     private double valor;
+    ConexionApis conversion;
+    Scanner lectura = new Scanner(System.in);
+    public Conversion(ConexionApis conversion) {
+        this.conversion = conversion;
+    }
 
     public String getMonedaOrigen() {
         return monedaOrigen;
@@ -17,18 +26,16 @@ public class Conversion {
         return valor;
     }
 
-    public void obtenerValores(String monedaBase, String monedaDestino, double valor){
+    public void obtenerValores(String monedaBase, String monedaDestino){
         this.monedaOrigen = monedaBase;
         this.monedaDestino = monedaDestino;
-        this.valor = valor;
+        System.out.println("Ingrese el valor que deseas convertir");
+        this.valor = Double.parseDouble(lectura.nextLine());
     }
 
-    @Override
-    public String toString() {
-        return "Conversor{" +
-                "monedaOrigen='" + monedaOrigen + '\'' +
-                ", monedaDestino='" + monedaDestino + '\'' +
-                ", valor=" + valor +
-                '}';
+    public String Mensajes() {
+        String mensaje = getMonedaOrigen().toUpperCase() + " " + getValor() + " equivale a: " + getMonedaDestino().toUpperCase() + " " + conversion.buscaConversion(getMonedaOrigen(), getMonedaDestino(), getValor());
+        System.out.println(mensaje);
+        return mensaje;
     }
 }
--- src/com/challegen/converso/principal/Main.java
+++ src/com/challegen/converso/principal/Main.java
@@ -1,28 +1,76 @@
 package com.challegen.converso.principal;
-
 import com.challegen.converso.modelos.Conversion;
 import com.challegen.converso.service.ConexionApis;
 
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
-        System.out.print("Ingrese el valor a convertir: ");
-        double valor = Double.parseDouble(lectura.nextLine());
-        String monedaOrigen = "COP";
-        String monedaDestino = "USD";
+        int opcionElegida = 0;
+        ConexionApis consulta = new ConexionApis();
+        Conversion calculos = new Conversion(consulta);
 
-        // Crear instancia de Conversor y establecer valores
-        Conversion conversor = new Conversion();
-        conversor.obtenerValores(monedaOrigen, monedaDestino, valor);
 
-        // Mostrar información del conversor
-        System.out.println(conversor);
+        String menu = """
+                \n
+                      *** Conversor de Monedas ***
+                ********** Menus de Opciones **********
+                
+                1) Peso Colombiano => Dólar Estadounidense
+                2) Peso Colombiano => Euro
+                3) Peso Colombiano => Bolivares 
+                4) Dólar Estadounidense => Peso Colombiano 
+                5) Euro => Peso Colombiano 
+                6) Bolivares => Peso Colombiano 
+                
+                8) Salir
+                ---------------------------------------------
+                """;
 
-        // Usar la clase ConexionApis para obtener la conversión
-        ConexionApis conexionApis = new ConexionApis();
-        String resultado = conexionApis.buscaConversion(monedaOrigen, monedaDestino, valor);
-        System.out.println("Resultado de la conversión: " + resultado);
+        while (opcionElegida != 8) {
+            try {
+                System.out.println(menu);
+                opcionElegida = Integer.parseInt(lectura.nextLine());
+
+
+                switch (opcionElegida) {
+                    case 1:
+                        calculos.obtenerValores("COP", "USD");
+                        System.out.println("Resultado de la conversion es: "+ calculos.Mensajes());
+                        break;
+                    case 2:
+                        calculos.obtenerValores("COP", "EUR");
+                        System.out.println("Resultado de la conversion es: " + calculos.Mensajes());
+                        break;
+                    case 3:
+                        calculos.obtenerValores("COP", "VES");
+                        System.out.println("Resultado de la conversion es: "+ calculos.Mensajes());
+                        break;
+                    case 4:
+                        calculos.obtenerValores("USD", "COP");
+                        System.out.println("Resultado de la conversion es: "+ calculos.Mensajes());
+                        break;
+                    case 5:
+                        calculos.obtenerValores("EUR", "COP");
+                        System.out.println("Resultado de la conversion es: "+ calculos.Mensajes());
+                        break;
+                    case 6:
+                        calculos.obtenerValores("VES", "COP");
+                        System.out.println("Resultado de la conversion es: " + calculos.Mensajes());
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        System.out.println("Ingrese una opción válida");
+                }
+            }  catch (NumberFormatException | InputMismatchException e) {
+                System.out.println("Error. Ingrese un valor numérico válido.");
+            }
+        }
+
+        System.out.println("Finalizando programa");
     }
 }
